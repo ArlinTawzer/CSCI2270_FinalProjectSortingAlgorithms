6fa05cd1b1b10eda9fea602d1e80c60ff657b563
Dmytro Ryzhkov <dmytro.ryzhkov@colorado.edu>
2015-04-28 00:31:40 -0600

    Information Menu
    Fixed numbering in the more information menu

--- Menus.cpp
+++ Menus.cpp
@@ -5,15 +5,6 @@
 
 using namespace std;
 
-/*
-Function Description:
-This function creates a new menu that has more functionality. Can be navigated to from the main menu if the user desires.
-This function also shows information about all the different sorts we have implemented thus far in the program. Mainly it briefly outlines the
-complexity for each sort.
-
-Pre-conditions: Nothing needs to be passed in for the function itself but within the function the menu depends on the user input.
-Post-conditions: Does not return or change any variable but it does display more information about each kind of sort.
-*/
 void Sorting::moreInformation() // use to give information on the sorting algorithms that we implement
 {
 	int input;
@@ -27,7 +18,7 @@ void Sorting::moreInformation() // use to give information on the sorting algori
         cout << "4. Quick Sort" << endl;
         cout << "5. Gnome Sort" << endl;
         cout << "6. Shell Sort"<<endl;
-		cout << "6. Go Back" << endl;
+		cout << "7. Go Back" << endl;
 
 		cin >> input;
         //clear out cin
@@ -76,24 +67,16 @@ void Sorting::moreInformation() // use to give information on the sorting algori
     }
 }
 
-/*
-Function Description:
-The settings function allows the user to change the array size as well as how the array is organized.
-This function was implemented to better understand how each sorted operated under different conditions.
-
-Pre-conditions: Requires nothing to be passed in since it only operates with global variables.
-Post-conditions: Changes different global variables all depending on the user input. It can change the array size or change how the array is organized.
-*/
 void Sorting::settings()
 {
     int input;
-    while(input != 3)
+    while(input != 5)
     {
         cout << "===========================================" << endl;
     	cout << "Welcome to the Settings menu, What would you like to do?" << endl;
 		cout << "1. Change Array Size" << endl;
 		cout << "2. Use Special Array Case" << endl;
-		cout << "3. Go Back" << endl;
+		cout << "5. Go Back" << endl;
 
 		cin >> input;
         //clear out cin
@@ -116,7 +99,7 @@ void Sorting::settings()
                 break;
             case 2:
                 cout << "===========================================" << endl;
-            	cout << "Which case would you like to use? Please keep in mind that the array must be initialized first." << endl;
+            	cout << "Which case would you like to use?" << array_size << endl;
             	cout << "1. Last and First values switched" << endl;
             	cout << "2. Already sorted" << endl;
             	cout << "3. Perfectly Reversed" << endl;
@@ -124,6 +107,10 @@ void Sorting::settings()
                 cin.clear();
                 cin.ignore(10000,'\n');
                 specialCases(input2);
+                break;
+            case 3:
+                break;
+            case 4:
                 break;
             case 5:
                 cout << "Goodbye!" << endl;
